4c1c4ac09e6458a44c852b531e82c3ce2e8ffd28
João Silva <joaosilva7875@gmail.com>
2025-02-15 22:49:19 +0100

    redux init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,8 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "TCG Collection" },
+    { name: "description", content: "Pokemon Trading Cards" },
   ];
 }
 
