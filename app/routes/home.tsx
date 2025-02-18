bd297d3c920fdf56ad8ce6a575d9fbec8de95748
João Silva <joaosilva7875@gmail.com>
2025-02-18 14:50:50 +0100

    header, built by me, github icon

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
 import type { Route } from "./+types/home";
+import OldTcgIcon from "../icons/oldTcgIcon.png";
+import GithubIcon from "../icons/githubIcon.png";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,5 +10,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <header className="flex flex-col justify-center items-center mt-2">
+        <div className="flex">
+          <span className="text-white mr-2">
+            Built by joaosilva7875@gmail.com
+          </span>
+          <img src={GithubIcon} className="w-5 h-5" />
+        </div>
+        <img src={OldTcgIcon} className="w-50 h-50 object-contain" />
+      </header>
+    </div>
+  );
 }
