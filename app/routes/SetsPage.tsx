56445bf3c6a9c1d3a4a6c434ceacf0ed28f33eab
João Silva <joaosilva7875@gmail.com>
2025-02-18 21:43:56 +0100

    axios fetch api set data console.log

--- app/routes/SetsPage.tsx
+++ app/routes/SetsPage.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import SetBanner from "~/components/SetBanner";
+import axios from "axios";
+import TCGdex from "@tcgdex/sdk";
 
 export default function SetsPage() {
+  const [sets, setSets] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchCard = async () => {
+      try {
+        const response = await axios.get("https://api.tcgdex.net/v2/en/sets");
+        setSets(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching card:", error);
+      }
+    };
+
+    fetchCard();
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center">
       <h2 className="mb-6">Browse through all the sets</h2>
