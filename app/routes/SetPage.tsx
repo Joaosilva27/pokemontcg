996718b31b85f5c9005e61b0b15b7688a0f71edd
João Silva <joaosilva7875@gmail.com>
2025-02-19 12:06:40 +0100

    cardCounts for each type of card

--- app/routes/SetPage.tsx
+++ app/routes/SetPage.tsx
@@ -23,14 +23,52 @@ export default function SetPage() {
 
   return (
     <div className="flex justify-center">
-      <div className="flex items-center">
-        <img
-          src={`${setCards.logo}.png`}
-          className="object-contain w-40 h-20 mr-5"
-        />
+      <div className="flex items-center flex-wrap justify-center">
+        {setCards.logo ? (
+          <img
+            src={`${setCards.logo}.png`}
+            className="object-contain w-40 h-20 mr-5"
+          />
+        ) : (
+          <span>Loading...</span>
+        )}
+
+        <div className="flex flex-col m-3">
+          <h1 className="text-xl font-bold">{setCards?.name}</h1>
+          <span>Release date: {setCards?.releaseDate}</span>
+          <span>Cards: {setCards?.cardCount?.total}</span>
+        </div>
         <div className="flex flex-col">
-          <h1 className="text-xl font-bold">{setCards.name}</h1>
-          <span></span>
+          <span>
+            First Ed. Cards:{" "}
+            {setCards?.cardCount?.firstEd != 0
+              ? setCards?.cardCount?.firstEd
+              : "None"}
+          </span>
+          <span>
+            Holo Cards:{" "}
+            {setCards?.cardCount?.holo != 0
+              ? setCards?.cardCount?.holo
+              : "None"}
+          </span>
+          <span>
+            Normal Cards:{" "}
+            {setCards?.cardCount?.normal != 0
+              ? setCards?.cardCount?.normal
+              : "None"}
+          </span>
+          <span>
+            Official Cards:{" "}
+            {setCards?.cardCount?.official != 0
+              ? setCards?.cardCount?.official
+              : "None"}
+          </span>
+          <span>
+            Reverse Cards:{" "}
+            {setCards?.cardCount?.reverse != 0
+              ? setCards?.cardCount?.reverse
+              : "None"}
+          </span>
         </div>
       </div>
     </div>
