dd7fe069815c803e2471dce9d8220b1d47a76332
João Silva <joaosilva7875@gmail.com>
2025-02-19 10:50:18 +0100

    fetch each set cards/details & page img banner and title

--- app/routes/SetPage.tsx
+++ app/routes/SetPage.tsx
@@ -1,3 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 export default function SetPage() {
-  return <div>setpage</div>;
+  const params = useParams();
+  const [setCards, setSetCards] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.tcgdex.net/v2/en/sets/${params.setId}`
+        );
+        setSetCards(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCards();
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      <div className="flex items-center">
+        <img
+          src={`${setCards.logo}.png`}
+          className="object-contain w-40 h-20 mr-5"
+        />
+        <div className="flex flex-col">
+          <h1 className="text-xl font-bold">{setCards.name}</h1>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
 }
