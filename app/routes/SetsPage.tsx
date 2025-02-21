4ecd7af599bbbed78309ba27512e47fd3e346182
João Silva <joaosilva7875@gmail.com>
2025-02-21 16:03:30 +0100

    added search for a set feature

--- app/routes/SetsPage.tsx
+++ app/routes/SetsPage.tsx
@@ -6,6 +6,10 @@ import { Link } from "react-router";
 
 export default function SetsPage() {
   const [sets, setSets] = useState<any>([]);
+  const [searchedSets, setSearchedSets] = useState<any>([]);
+  const [inputSearch, setInputSearch] = useState<any>();
+  const [noSetFound, setNoSetFound] = useState<boolean>(false);
+  const [isUserSearching, setIsUserSearching] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchCard = async () => {
@@ -21,9 +25,31 @@ export default function SetsPage() {
     fetchCard();
   }, []);
 
+  const onSetSearch = (e) => {
+    e.preventDefault();
+
+    const getPokemonCards = async () => {
+      if (inputSearch != "") {
+        setIsUserSearching(false);
+      }
+      const fetchCards = await axios.get(
+        `https://api.tcgdex.net/v2/en/sets?name=${inputSearch}`
+      );
+      setSearchedSets(fetchCards.data);
+      setNoSetFound(false);
+      setIsUserSearching(true);
+      console.log(fetchCards.data);
+      if (fetchCards.data.length == 0) {
+        setNoSetFound(true);
+      }
+    };
+
+    getPokemonCards();
+    setInputSearch("");
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
-      <h2>Browse through all the sets</h2>
       <h3 className="text-xs text-center">
         I will not add a 'filter by year' feature because that would require me
         to update the app constantly, and this is just a homework project.{" "}
@@ -31,17 +57,45 @@ export default function SetsPage() {
         The sets however, will always be updated automatically as soon as a new
         set is released (thanks to TcgDex API).
       </h3>
-      <div className="flex flex-wrap m-6 justify-center items-center">
-        {sets.map((set) => (
-          <Link to={`/sets/${set.id}`}>
-            <SetBanner
-              setTitle={set.name}
-              cardAmount={set.cardCount.total}
-              imageUrl={set.logo ? `${set.logo}.png` : FallbackImageErrorIcon}
-            />
-          </Link>
-        ))}
-      </div>
+      <form className="flex items-center justify-center mt-3">
+        <input
+          value={inputSearch}
+          onChange={(e) => setInputSearch(e.target.value)}
+          placeholder="Search for a set..."
+          className="bg-gray-50 border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        ></input>
+        <button
+          onClick={onSetSearch}
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 ml-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+          Search
+        </button>
+      </form>
+      {isUserSearching == false ? (
+        <div className="flex flex-wrap m-6 justify-center items-center">
+          {sets.map((set) => (
+            <Link to={`/sets/${set.id}`}>
+              <SetBanner
+                setTitle={set.name}
+                cardAmount={set.cardCount.total}
+                imageUrl={set.logo ? `${set.logo}.png` : FallbackImageErrorIcon}
+              />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-wrap m-6 justify-center items-center">
+          {searchedSets.map((set) => (
+            <Link to={`/sets/${set.id}`}>
+              <SetBanner
+                setTitle={set.name}
+                cardAmount={set.cardCount.total}
+                imageUrl={set.logo ? `${set.logo}.png` : FallbackImageErrorIcon}
+              />
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <nav className="flex justify-center mb-4">
         <Link to="/search">
           <span className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 ml-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-            Search
+            Cards
           </span>
         </Link>
         <Link to="/sets">
